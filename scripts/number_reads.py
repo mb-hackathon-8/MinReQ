a00bc8fe6ee8c320b4e2b7a7bdcced308a60d414
boasvdp <boasvdp@gmail.com>
2022-09-13 15:43:37 +0200

    update number_reads.py

--- scripts/number_reads.py
+++ scripts/number_reads.py
@@ -6,5 +6,5 @@ import sys
 command = ' '.join(["seqtk comp", sys.argv[1], "| awk '{t += $2} END {print t}'"])
 subprocess_out = subprocess.run(command, capture_output=True, shell=True)
 genome_size = int(subprocess_out.stdout.decode("utf-8").rstrip('\n').split('\n')[0])
-n_reads = genome_size * 50 / 600
+n_reads = genome_size * 50 / 300
 print(int(round(n_reads, 0)))
